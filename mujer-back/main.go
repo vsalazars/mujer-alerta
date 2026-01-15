3c9978e2a97d31ae379fc6d860ed4ac5b563d9ae
Vidal Salazar <vidalsalazarsanchez@gmail.com>
2026-01-15 01:48:31 -0600

    scroll en móviles pantalla preguntas

--- mujer-back/main.go
+++ mujer-back/main.go
@@ -306,7 +306,8 @@ func main() {
 			// ✅ Vercel (producción + previews)
 			"https://mujer-alerta.vercel.app",
 			"https://mujer-alerta-git-main-vidal-salazars-projects.vercel.app",
-			"https://mujer-alerta-k388o6kze-vidal-salazars-projects.vercel.app",
+			
+			"https://mujer-alerta-roegsr583-vidal-salazars-projects.vercel.app",
 			
 
 		},
