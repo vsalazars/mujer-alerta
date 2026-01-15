a3dc752a78bc2f9729d4f403039b52dd1749b1de
Vidal Salazar <vidalsalazarsanchez@gmail.com>
2026-01-15 04:03:58 -0600

    cards de resultados usuario back

--- mujer-back/main.go
+++ mujer-back/main.go
@@ -306,8 +306,8 @@ func main() {
 			// ✅ Vercel (producción + previews)
 			"https://mujer-alerta.vercel.app",
 			"https://mujer-alerta-git-main-vidal-salazars-projects.vercel.app",
-			"https://mujer-alerta-8hu3u0605-vidal-salazars-projects.vercel.app",
-									
+			"https://mujer-alerta-iazm3hyx0-vidal-salazars-projects.vercel.app",
+												
 		},
 		AllowedMethods: "GET, POST, PUT, DELETE, OPTIONS",
 		AllowedHeaders: "Content-Type, Authorization",
